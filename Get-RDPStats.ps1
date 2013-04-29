78b0b52c06152c8dd53254acf15f4298ae37a059
Craig Meinschein <cmeinsch@gmail.com>
2013-04-29 12:34:46 -0700

    Updated version of RDP connections statistics script.

--- Get-RDPStats.ps1
+++ Get-RDPStats.ps1
@@ -1,12 +1,13 @@
-﻿## get-UserCount.ps1
+﻿## Get-RDPStats.ps1
 ## Sandeep S Parmar
 ## 2/8/2013
 ## This is used to grab the total count and the active on the server and send the counts to graphite. 
 
-$compname = $env:COMPUTERNAME
+$compname = $env:COMPUTERNAME.tolower()
 
 ## Used to get the infomration who is loggedon and which users are active. 
-$path=\\cecs\resources\programs\programs\tools\psloggedon.exe \\$compname 2>&1 | where{$_ -match "cecs"} | where{$_ -notmatch $env:USERNAME}
+reg add HKCU\Software\Sysinternals\loggedon /v EulaAccepted /t REG_DWORD /d 1 /f | Out-Null
+$path=C:\Windows\Graphite\psloggedon.exe \\$compname 2>&1 | where{$_ -match "cecs"} | where{$_ -notmatch $env:USERNAME}
 $active = query user /server:$compname | ?{$_ -match "\b$UserName\b"} | ?{$_ -match "rdp-tcp#\d*"}
 
 ## Used the get the total count. 
@@ -26,8 +27,8 @@ $carbonServerPort = 2003
 $epochTime=[int](Get-Date -UFormat "%s") + 28800
 
 ## Formatted for graphite
-$totalusers = ("wintel.ts_cecs_pdx_edu.totalusers " + $totalcount + " " + $epochTime)
-$activeusers = ("wintel.ts_cecs_pdx_edu.activeusers " + $activecount + " " + $epochTime)
+$totalusers = ("wintel.terminal_server.${compname}_ds_cecs_pdx_edu.totalusers " + $totalcount + " " + $epochTime)
+$activeusers = ("wintel.terminal_server.${compname}_ds_cecs_pdx_edu.activeusers " + $activecount + " " + $epochTime)
 
 #Stream results to the Carbon server
 $socket = New-Object System.Net.Sockets.TCPClient 
